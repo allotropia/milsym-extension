dcc0edc712654bd0b3b3b786c2edb89ffb03e46d
Thorsten Behrens <thorsten.behrens@collabora.com>
2025-11-01 04:19:16 +0100

    Revert "Temporarily revert to milsymbol 2.2.0"
    This reverts commit d2af12111e66ccef2ea7555de60d49dd3f4defbb.

--- milsymbol/combine.sh
+++ milsymbol/combine.sh
@@ -9,15 +9,30 @@
 set -e  # Exit on any error
 
 # Downloaded from https://github.com/spatialillusions/milsymbol/releases/tag/v3.0.3
-INPUT_FILE="milsymbol-2.2.0.js"
+INPUT_FILE="milsymbol-3.0.3.js"
 OUTPUT_FILE="milsymbol.js"
 
 cp $INPUT_FILE $OUTPUT_FILE
 
+# Replace 'const' and 'let' with 'var'
+sed -i -E -e 's/\<const|let\>/var/g' $OUTPUT_FILE
+
+sed -e 's/^export default {/const pathsReinforced = {/' \
+ paths-reinforced.js >> $OUTPUT_FILE
+sed -E -e 's|^import pathsReinforced from "./paths-reinforced";||' \
+ -e 's/^export default function sigintReinforced/function sigintReinforced/' -e 's/\<let\>/var/g' \
+ sigint-reinforced-specialheadquarter.js >> $OUTPUT_FILE
+sed -E -e 's/^export default function stack/function stack/' -e 's/\<let\>/var/g' \
+ stack-extension.mjs >> $OUTPUT_FILE
 cat country-flags.js >> $OUTPUT_FILE
 
 # Append the final code to expose the main functionality
 cat >>$OUTPUT_FILE <<\EOF
+const parts = ms.getSymbolParts();
+parts.splice(6, 0, sigintReinforced); // Insert sigintReinforced in the begining
+parts.unshift(stack); // Insert stack at the begining
+ms.setSymbolParts(parts);
+
 var options = {};
 for (var i = 1; i < ARGUMENTS.length; ++i) {
     var name = ARGUMENTS[i].Name;
