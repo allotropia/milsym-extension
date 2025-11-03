e9802a69c9d7bbbc31104b903b8ddd35199be4ae
Thorsten Behrens <thorsten.behrens@collabora.com>
2025-11-03 05:40:31 +0100

    Update license and copyright notices
    - add copyright text and MPL exhibit A text to all relevant files
    - add COPYING and THIRDPARTYLICENSE with authorship notices and
      licensing details

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# SPDX-FileCopyrightText: Collabora Productivity and contributors
+#
 # SPDX-License-Identifier: MPL-2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # Build script for LibreOffice extension
 # Creates a .oxt file from the folder contents, excluding README and build artifacts
--- milsymbol/combine.sh
+++ milsymbol/combine.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# SPDX-FileCopyrightText: Collabora Productivity and contributors
+#
 # SPDX-License-Identifier: MPL-2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # This script combines the contents of the milsymbol directory into a single Javascript file.
 # Additionally, some statements are modified to ensure compatibility with the rhino version
