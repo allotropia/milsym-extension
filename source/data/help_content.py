5fe25e293fd9a775bb50c29c162298a924f1b9bf
Rashesh Padia <rashesh.padia@collabora.com>
2026-01-12 12:32:54 +0530

    symbol_dialog: translate help button text

--- source/data/help_content.py
+++ source/data/help_content.py
@@ -20,65 +20,11 @@ with an id starting with "btHelp" by looking up the field name in this dictionar
 
 HELP_CONTENT = {
     "EngageBarText": {
-        "title": "Engagement Bar Text Help",
-        "message": """The engagement amplifier shall be arranged as follows: A:BBB-CC
-
-A (1 character) - Local vs Remote Engagement:
-  (blank) = Local engagement (assigned to ownship)
-  R = Remote engagement (assigned outside ownship control)
-  B = Both local and remote engagements
-
-BBB (up to 3 characters) - Engagement Stage:
-  ASN = Assign/Cover
-  ENG = Engage
-  MIF = Missile in Flight
-  CF = Cease Fire
-  CE = Cease Engage
-  HF = Hold Fire
-  TE = Terminate Engagement
-  BE = Break Engagement
-  MBE = Management by Exception
-  M<T = MBE Less Than Threshold
-  MLT = Multiple Engagements
-
-CC (up to 2 characters) - Weapon/Asset:
-  M = Missile
-  BM = Ballistic Missile
-  CM = Cruise Missile
-  GN = Gun
-  T = Torpedo
-  A = Attack Aircraft
-  C = Combat Air Patrol (Defensive Counter Air)
-  D = Defensive Counter Air (Combat Air Patrol)
-  UW = USW/ASW Engagement
-  MW = Mine Warfare Engagement
-  SW = Surface Warfare Engagement
-  EA = Electronic Attack
-  ED = Electronic Defense
-  UV = Unmanned Vehicle
-  CW = Close-In Weapon System
-  L3 = LAMPS
-  VA = Vertical Launch ASROC
-  ## = Number of Engagements (02-99)""",
+        "title": "Help.EngageBarText.Title",
+        "message": "Help.EngageBarText.Message",
     },
     "EvaluatRating": {
-        "title": "Evaluation Rating Help",
-        "message": """A text amplifier for units, equipment and installations that consists of a one-letter reliability rating and a one-number credibility rating.
-
-Reliability Ratings:
-  A - Completely reliable
-  B - Usually reliable
-  C - Fairly reliable
-  D - Not usually reliable
-  E - Unreliable
-  F - Reliability cannot be judged
-
-Credibility Ratings:
-  1 - Confirmed by other sources
-  2 - Probably true
-  3 - Possibly true
-  4 - Doubtfully true
-  5 - Improbable
-  6 - Truth cannot be judged""",
+        "title": "Help.EvaluatRating.Title",
+        "message": "Help.EvaluatRating.Message",
     },
 }
